758eeae1ec7c8741eaef3aff4872d7edac592068
Nicole Ocholla <nicoleochola@gmail.com>
2020-02-17 12:17:32 +0300

    Created a view function for the index.html

--- app/views.py
+++ app/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.templatetags.static import static
+from django.shortcuts import render, redirect, render_to_response, HttpResponseRedirect
+from django.http import HttpResponse, Http404
+import datetime as dt
+from .models import *
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from .forms import *
+from django.contrib import messages
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import ProfileSerializer, ProjectSerializer
 
-# Create your views here.
+def index(request):
+  if request.method == 'POST':
+    from = RegisterForm(request.POST)
+    if form.is_valid():
+      form.save()
+      username = form.cleaned_data.get('username')
+      messages.success(request, f'Account created for {username}')
+      return redirect('/')
+  else:
+    form = RegisterForm()
+  return render(request, 'registration/registration_form.html', {'form':form})
+   
