6db2e423cdb11af632966d0f9f728ef77e6f7f4d
Nicole Ocholla <nicoleochola@gmail.com>
2020-02-17 09:54:31 +0300

    Dowloaded Django

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/c/Users/nicol/Moringa/Python/Django/Reviews/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
