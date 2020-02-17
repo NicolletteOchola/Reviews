93aa7a03ef22cfb7c4fd17bc2a5e941e9fa0f0bb
Nicole Ocholla <nicoleochola@gmail.com>
2020-02-17 12:51:43 +0300

    Added the two classes for the API view

--- app/views.py
+++ app/views.py
@@ -83,3 +83,15 @@ def new_project(request):
     form = NewProjectForm()
   return render(request, 'new-project.html', {"form": form})
 
+class ProjectList(APIView):
+  def get(self, request, format=None):
+    all_project = Projects.objects.all()
+    serializers = ProjectSerializer(all_project, many=True)
+    return Response(serializers.data)
+
+class ProfileList(APIView):
+  def(self, request, format=None):
+    all_profile = Profile.objects.all()
+    serializers = ProfileSerializer(all_profile, many=True)
+    return Response(serializers.data)
+    
