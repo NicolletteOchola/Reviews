5d4cc6974c0db02fc4b82671a76ad3b3621a5290
Nicole Ocholla <nicoleochola@gmail.com>
2020-02-17 12:23:57 +0300

    Added a view function for searching the projects

--- app/views.py
+++ app/views.py
@@ -28,3 +28,17 @@ def register(request):
   else:
     form = RegisterForm()
   return render(request, 'registration/registration_form.html', {'form':form})
+
+@login_required(login_url='/accounts/login/')
+def search_projects(request):
+  if 'keyword' in request.GET and request.GET["keyword"]:
+    search_term = request.GET.get("keyword")
+    searched_projects = Projects.search_projects(search_term)
+    message = f"{search_term}"
+    return render(request, 'search.html', {"message":message,"projects": searched_projects})
+
+  else:
+    message = "No term has been searched"
+    return render(request, 'search.html', {"message": message})
+
+
