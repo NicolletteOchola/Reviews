7c79578d0ca6d0b690a5b4bd5f75630281a7f456
Nicole Ocholla <nicoleochola@gmail.com>
2020-02-17 11:12:34 +0300

    Package installation

--- virtual/bin/django-admin.py
+++ virtual/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/mnt/c/Users/nicol/Moringa/Python/Django/Reviews/virtual/bin/python3.6
+#!/mnt/c/Users/nicol/Moringa/Python/Django/Reviews/virtual/bin/python
 from django.core import management
 
 if __name__ == "__main__":
