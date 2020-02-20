1549018d2f98d932cee069b00c12a275099c6234
Nicole Ocholla <nicoleochola@gmail.com>
2020-02-20 11:56:38 +0300

    Added a test for the creaton of a user

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import *
+from django.db import models
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class ProfileTest(TestCase):
+  def setUp(self):
+    self.nicole= User(first_name = 'Nicole', last_name = 'Ochola', email = 'nicoleochola@gmail.com')
+  
+  def test_profile_creation(self):
+    self.assertTrue(isinstance(self.nicole,User))
+
+
+
+
